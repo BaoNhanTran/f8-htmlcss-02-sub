44e93f66dc47aa0bfb426514cbc7a1cbbd57f7e7
BaoNhanTran <tranxuandinh181092@gmail.com>
2025-04-01 15:43:20 +0700

    make hero element

--- indes.js
+++ indes.js
@@ -1,9 +1,11 @@
 const logo = document.querySelector('.logo');
 const nav = document.querySelector('.nav');
 const ctaGroup = document.querySelector('.cta-group');
-const rect1 = logo.getBoundingClientRect();
-const rect2 = nav.getBoundingClientRect();
+const heroContent = document.querySelector('.hero .content');
+const heroVisual = document.querySelector('.hero .visual');
+const rect1 = heroContent.getBoundingClientRect();
+const rect2 = heroVisual.getBoundingClientRect();
 const rect3 = ctaGroup.getBoundingClientRect();
 
-const distance = rect3.left - rect2.right;
+const distance = rect2.left - rect1.right;
 console.log(distance);
